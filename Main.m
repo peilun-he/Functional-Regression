406ee7f68fa4099e333ffc29b41efab48252069e
peilun-he <63130900+peilun-he@users.noreply.github.com>
2021-07-17 11:33:05 +0800

    Update Main.m

--- Main.m
+++ Main.m
@@ -1,7 +1,7 @@
 clear;
 addpath(genpath(pwd));
 load('FuncAnaData.mat')
-
+ 
 % data
 chi = af(:, 1);
 xi = af(:, 2);
@@ -29,29 +29,29 @@ if (L2-1)*n2 > N
 end
 knot1 = [repelem(1, m1), (1: L1-1) * n1, repelem(N, m1)];
 knot2 = [repelem(1, m2), (1: L2-1) * n2, repelem(N, m2)];
-phi = zeros(N, L1+m1-1);
-theta = zeros(N, L2+m2-1);
+B1 = zeros(N, L1+m1-1); % B_{i,m1}^{(1)}, the basis for functions chi(t) and xi(t)
+B2 = zeros(N, L2+m2-1); % B_{j,m2}^{(2)}, the basis for functions alpha1(t) and alpha2(t)
 
 for i = 1: L1+m1-1
-    phi(:, i) = B_Spline(i, knot1, m1);
+    B1(:, i) = B_Spline(i, knot1, m1);
 end
 
 for i = 1: L2+m2-1
-    theta(:, i) = B_Spline(i, knot2, m2);
+    B2(:, i) = B_Spline(i, knot2, m2);
 end
 
 % stage 1: functional analysis for chi and xi 
-D2phi = zeros(N-2, L1+m1-1); % second order derivative of phi 
+D2basis1 = zeros(N-2, L1+m1-1); % second order derivative of phi 
 for i = 2: N-1
-    D2phi(i-1, :) = (phi(i+1, :) - 2*phi(i, :) + phi(i-1, :))./ deltat^2 ;
+    D2basis1(i-1, :) = (B1(i+1, :) - 2*B1(i, :) + B1(i-1, :))./ deltat^2 ;
 end
-R1 = D2phi' * D2phi * deltat; 
+R1 = D2basis1' * D2basis1 * deltat; 
 
-c_hat_chi = inv(phi' * phi + lambda1 * R1) * phi' * chi;
-c_hat_xi = inv(phi' * phi + lambda1 * R1) * phi' * xi;
+c_hat_chi = inv(B1' * B1 + lambda1 * R1) * B1' * chi;
+c_hat_xi = inv(B1' * B1 + lambda1 * R1) * B1' * xi;
 
-chi_hat = phi * c_hat_chi; 
-xi_hat = phi * c_hat_xi;
+chi_hat = B1 * c_hat_chi; % chi(t)
+xi_hat = B1 * c_hat_xi; % xi(t)
 c_hat = [c_hat_chi, c_hat_xi];
 
 if ifPlot
@@ -81,14 +81,14 @@ ub = [];
 %c = @(x) []; % c(x) <= 0 
 %ceq = @(x) []; % ceq(x) = 0
 %nonlcon = @(x) deal(c(x), ceq(x));
-nonlcon = @(x) Fix1End(x, y, chi, xi, lambda2, phi, c_hat, theta, deltat, false);
+nonlcon = @(x) Fix1End(x, y, chi, xi, lambda2, B1, c_hat, B2, deltat, 2);
 
-func_sse = @(c) SSE_Penalty_FR(c, y, chi, xi, lambda2, phi, c_hat, theta, deltat);
+func_sse = @(c) SSE_Penalty_FR(c, y, chi, xi, lambda2, B1, c_hat, B2, deltat);
 par0 = repelem(1, 2*(L2+m2-1)+3);
 options = optimoptions(@fmincon, 'TolFun', 1e-06, 'TolX', 1e-06, 'MaxIter',100000, 'MaxFunEvals', 200000);
 [coe, fval, ~] = fmincon(func_sse, par0, A, b, Aeq, beq, lb, ub, nonlcon, options);
 
-[SSE, y_hat] = SSE_Penalty_FR(coe, y, chi, xi, lambda2, phi, c_hat, theta, deltat);
+[SSE, y_hat] = SSE_Penalty_FR(coe, y, chi, xi, lambda2, B1, c_hat, B2, deltat);
 
 if ifPlot
     figure;
@@ -98,7 +98,5 @@ if ifPlot
     legend(["y", "y hat"]);
     hold off;
 end
-
-
 
 
